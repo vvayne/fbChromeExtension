4709ef5714f3bd89eea853c2c855faef8af5f91d
Wayne S. <wayneshoe@gmail.com>
2014-08-19 17:35:22 -0400

    Just pruned a lot of things right now. Testing proof of concept

--- minimize.js
+++ minimize.js
@@ -1,3 +1,6 @@
 function pruneAndPrint(){
-    $( "#pagelet_welcome_box" ).css( "display","none");
+    $( "#pagelet_welcome_box,#pagelet_navigation, #rightCol, #stream_pagelet").css( "display","none");
+    $( "#mainContainer").css("margin", "10px 150px auto 165px");
 }
+
+pruneAndPrint();
